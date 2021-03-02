1b45368e51d077c33e61107980b8cba1e8f88a51
Circe Russell <circeroserussell@gmail.com>
2021-03-01 19:24:33 -0500

    added validate session/token

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const cocktail = require('./controllers/cocktailcontroller')
 
 sequelize.sync()
 //sequelize.sync({force:true}) - THIS RESETS DB
+app.use(cors())
+app.use(require("./middleware/headers"));
+app.use(express.json())
 
 app.use('/bartender', user)
 
-//will be protected route (need to add in validate-session later)
-//app.use(require("./middleware/validate-session"));
+app.use(require("./middleware/validate-session"));
 app.use('/mybar', cocktail)
 
 app.listen(3000, () => {
