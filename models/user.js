44e89ea4ad3c17cc3a457c25b0c00b5b3381a212
Chelsey <chelsey.tschida@outlook.com>
2021-03-06 10:47:06 -0500

    deleted commented out code x2

--- models/user.js
+++ models/user.js
@@ -1,10 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
-        //these are columns in the table
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true //only one email allowed
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
